00184ef5c9c4232c21e1193696f119cd39011648
Yumu0112 <yumu0112@icloud.com>
2023-06-15 14:23:58 +0900

    set up

---
+++ src/StreamPractice.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class StreamPractice {
+    public static void main(String[] args) {
+        List<String> stations = List.of("Shibuya", "Ebisu", "Meguro", "Gotanda");
+
+
+        System.out.println("eが含まれる駅名");
+        List<String> result = stations.stream().filter(station -> station.contains("e")).toList();
+        System.out.println(result);
+
+        System.out.println("aが含まれる駅名");
+        stations.stream().filter(station -> station.contains("a")).forEach(System.out::println);
+
+        System.out.println("大文字に並び替え");
+        stations.stream().map(String::toUpperCase).forEach(System.out::println);
+
+    }
+
+}
