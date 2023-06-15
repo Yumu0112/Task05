694e6d0a28c19503d53c5e87966ae2453729de4a
Yumu0112 <yumu0112@icloud.com>
2023-06-15 15:58:35 +0900

    第五回課題用コメントアウト

--- src/StreamPractice.java
+++ src/StreamPractice.java
@@ -1,19 +1,21 @@
 import java.util.List;
+import java.util.Map;
 
 public class StreamPractice {
     public static void main(String[] args) {
         List<String> stations = List.of("Shibuya", "Ebisu", "Meguro", "Gotanda");
 
 
-        System.out.println("eが含まれる駅名");
-        List<String> result = stations.stream().filter(station -> station.contains("e")).toList();
-        System.out.println(result);
+//        コメントアウト
+//        System.out.println("eが含まれる駅名");
+//        List<String> result = stations.stream().filter(station -> station.contains("e")).toList();
+//        System.out.println(result);
 
         System.out.println("aが含まれる駅名");
         stations.stream().filter(station -> station.contains("a")).forEach(System.out::println);
 
-        System.out.println("大文字に並び替え");
-        stations.stream().map(String::toUpperCase).forEach(System.out::println);
+//        System.out.println("大文字に並び替え");
+//        stations.stream().map(String::toUpperCase).forEach(System.out::println);
 
     }
 
